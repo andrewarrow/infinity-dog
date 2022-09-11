d3a3f6530add4b4008fd61247860dee49bb90872
Andrew Arrow <oneone@gmail.com>
2022-09-11 15:35:51 -0700

    times

--- dog/logs.go
+++ dog/logs.go
@@ -4,30 +4,46 @@ import (
 	"encoding/json"
 	"fmt"
 	"infinity-dog/network"
+	"time"
 )
 
+func golangTimeToDogTime(s string) string {
+	dateString := s[0:10]
+	timeString := s[11:19]
+	return dateString + "T" + timeString
+}
+
 func Logs(query string) {
+
+	utc, _ := time.LoadLocation("UTC")
+	utcNow := time.Now().In(utc)
+	utcString := fmt.Sprintf("%v", utcNow.Add(time.Second*-60))
+	from := golangTimeToDogTime(utcString)
+	utcString = fmt.Sprintf("%v", utcNow.Add(time.Second))
+	to := golangTimeToDogTime(utcString)
+
 	payload := `{
   "filter": {
     "query": "%s",
     "indexes": [
       "main"
     ],
-    "from": "2022-09-10T00:00:00+01:00",
-    "to": "2022-09-12T23:59:59+01:00"
+		"from": "%s+01:00",
+    "to": "%s+01:00"
   },
   "sort": "timestamp",
   "page": {
-    "limit": 50
+	  "cursor": null,
+    "limit": 500
   }
 }`
-	payloadString := fmt.Sprintf(payload, query)
+	payloadString := fmt.Sprintf(payload, query, from, to)
 	jsonString := network.DoPost("/api/v2/logs/events/search", []byte(payloadString))
 
 	var logResponse LogResponse
 	json.Unmarshal([]byte(jsonString), &logResponse)
 
 	for _, d := range logResponse.Data {
-		fmt.Println(d.Attributes.Service)
+		fmt.Println(d.Attributes.Timestamp, d.Attributes.Service)
 	}
 }
