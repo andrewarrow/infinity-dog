9d7d58e66d4f44267d20949385a59112807084cf
Andrew Arrow <oneone@gmail.com>
2022-09-15 11:56:25 -0700

    sorting by hits

--- dog/services.go
+++ dog/services.go
@@ -5,7 +5,13 @@ import (
 	"fmt"
 	"infinity-dog/files"
 	"io/ioutil"
+	"sort"
 )
+
+type Service struct {
+	Name string
+	Hits int
+}
 
 func Services() {
 	sampleFiles, err := ioutil.ReadDir("samples")
@@ -14,18 +20,30 @@ func Services() {
 		return
 	}
 
-	services := map[string]int{}
+	servicesMap := map[string]int{}
 
 	for _, file := range sampleFiles {
 		jsonString := files.ReadFile("samples/" + file.Name())
 		var logResponse LogResponse
 		json.Unmarshal([]byte(jsonString), &logResponse)
 		for _, d := range logResponse.Data {
-			services[d.Attributes.Service]++
+			servicesMap[d.Attributes.Service]++
 		}
 	}
 
-	for k, v := range services {
-		fmt.Println(k, v)
+	services := []Service{}
+
+	for k, v := range servicesMap {
+		s := Service{k, v}
+		services = append(services, s)
 	}
+
+	sort.SliceStable(services, func(i, j int) bool {
+		return services[i].Hits > services[j].Hits
+	})
+
+	for i, service := range services {
+		fmt.Printf("%03d. %-60s %d\n", i+1, service.Name, service.Hits)
+	}
+
 }
